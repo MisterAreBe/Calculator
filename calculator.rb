0780f4d4b35673e589d05927406f57c6d0febdc3
Ricky <richardcollins52317@gmail.com>
2018-06-11 11:30:24 -0400

    full calculator

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,33 @@
-def addition()
-    puts "Enter first number"
-    print "> "
-    x = $stdin.gets.chomp
+def calculator()
+    while true
+        puts "Enter first number"
+        print "> "
+        x = $stdin.gets.chomp.to_i
 
-    puts "Enter second number"
-    print "> "
-    y = $stdin.gets.chomp
+        puts "Enter second number"
+        print "> "
+        y = $stdin.gets.chomp.to_i
 
-    print "#{x} + #{y} = "
-    puts x.to_i + y.to_i
+        puts "Would you like to \"add\", \"subtact\", \"multiply\", \"divide\", or \"quit\"?"
+        print "> "
+        z = $stdin.gets.chomp
+    
+        if z == "add"
+            print "#{x} + #{y} = "
+            puts x + y
+        elsif z == "subtract"
+            print "#{x} - #{y} = "
+            puts x - y
+        elsif z == "multiply"
+            print "#{x} * #{y} = "
+            puts x * y
+        elsif z == "divide"
+            print "#{x} / #{y} = "
+            puts x / y
+        elsif z == "quit"
+            break
+        end
+    end
 end
 
-addition()
+calculator()
