4d7c787ed21fef33ad4ad5f28dc1974b52a467c3
Ricky <richardcollins52317@gmail.com>
2018-06-12 22:24:00 -0400

    Shorter calculator, hope this works

--- calculator.rb
+++ calculator.rb
@@ -1,33 +1,21 @@
-def calculator()
-    while true
-        puts "Enter first number"
-        print "> "
-        x = $stdin.gets.chomp.to_i
-
-        puts "Enter second number"
-        print "> "
-        y = $stdin.gets.chomp.to_i
-
-        puts "Would you like to \"add\", \"subtact\", \"multiply\", \"divide\", or \"quit\"?"
-        print "> "
-        z = $stdin.gets.chomp
-    
-        if z == "add"
-            print "#{x} + #{y} = "
-            puts x + y
-        elsif z == "subtract"
-            print "#{x} - #{y} = "
-            puts x - y
-        elsif z == "multiply"
-            print "#{x} * #{y} = "
-            puts x * y
+while true
+    puts "Enter first number."; print "> "
+    x = $stdin.gets.chomp
+    puts "Enter second number."; print "> "
+    y = $stdin.gets.chomp
+    puts "Would you like to? [Add] [Subtract] [Multiply] [Divide]"; print "> "
+    z = $stdin.gets.chomp
+    if (x =~ /\A[[:digit:]]+(\.[[:digit:]]+)?\Z/x) && (y =~ /\A[[:digit:]]+(\.[[:digit:]]+)?\Z/x)
+        x = x.to_f; y = y.to_f
+        if z == "add"; puts "#{x} + #{y} = #{x + y}"
+        elsif z == "subtract"; puts "#{x} - #{y} = #{x - y}"
+        elsif z == "multiply"; puts "#{x} * #{y} = #{x * y}"
         elsif z == "divide"
-            print "#{x} / #{y} = "
-            puts x / y
-        elsif z == "quit"
-            break
+            if (x == 0) || (y == 0); puts "You can't divide by zero, try not making a blakchole."
+            else; puts "#{x} / #{y} = #{x / y}"
+            end
         end
+    elsif z == "quit"; break
+    else; puts "I can't work with letters, try a number!"
     end
 end
-
-calculator()
